c98bfc14be3ce4f50a0eee5a2b7fd31d8fce3861
Rakshith Kumar S <rakshithkumars7777@gmail.com>
2021-02-02 23:28:30 +0530

    camera and image picker is added

--- components/main/Add.js
+++ components/main/Add.js
@@ -1,21 +1,124 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity ,Button,Image } from 'react-native';
+import { Camera } from 'expo-camera';
+import * as ImagePicker from 'expo-image-picker';
+import { Ionicons,MaterialIcons,Fontisto } from 'react-native-vector-icons' 
 
-const Add = () => {
-    return (
-        <View style={styles.container}>
-            <Text>AddScreen</Text>
+export default function App() {
+  const [hasGalleryPermission, setHasGalleryPermission] = useState(null);
+  const [hasCameraPermission, setHasCameraPermission] = useState(null);
+  const [camera, setCamera] = useState(null);
+  const [image, setImage] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+
+  useEffect(() => {
+    (async () => {
+      const cameraStatus = await Camera.requestPermissionsAsync();
+      setHasCameraPermission(cameraStatus.status === 'granted');
+
+      const galleryStatus = await ImagePicker.requestCameraRollPermissionsAsync();
+      setHasGalleryPermission(galleryStatus.status === 'granted');
+    })();
+  }, []);
+
+  const takePicture = async () => {
+    if (camera) {
+      const data = await camera.takePictureAsync(null);
+      // console.log(data.uri);
+      setImage(data.uri);
+    }
+  }
+   const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [1, 1],
+      quality: 1,
+    });
+    console.log(result);
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
+
+  
+  if (hasCameraPermission === null || hasGalleryPermission === false) {
+    return <View />;
+  }
+  if (hasCameraPermission === false || hasGalleryPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+  return (
+    <View style={{ flex: 1,flexDirection:'column-reverse' }}>
+      <Camera style={styles.camera} type={type}>
+        <View style={styles.buttonContainer}>
+          <Ionicons name="ios-camera-reverse-outline" color={"white"} size={38} onPress={() => {
+            setType(
+              type === Camera.Constants.Type.back
+                ? Camera.Constants.Type.front
+                : Camera.Constants.Type.back
+            );
+          }}/>
+          <Ionicons name="ios-radio-button-on" color={"white"} size={75} onPress={() => takePicture()} />
+          <Fontisto name="photograph" color={"white"} size={38} onPress={() => pickImage()}/>
         </View>
-    )
+      </Camera>
+      {image && <Image source={{ uri: image }} style={{ flex: 1 }} />} 
+      {/* <View style={styles.cameraContainer}>
+        <Camera
+          ref={ref => setCamera(ref)}
+          style={styles.fixedRatio}
+          type={type}
+          ratio={'1:1'} />
+      </View>
+
+      <Button
+        title="Flip Image"
+        onPress={() => {
+          setType(
+            type === Camera.Constants.Type.back
+              ? Camera.Constants.Type.front
+              : Camera.Constants.Type.back
+          );
+        }}>
+      </Button>
+      <Button title="Take Picture" onPress={() => takePicture()} />
+      <Button title="Pick Image From Gallery" onPress={() => pickImage()} />
+      {/* <Button title="Save" onPress={() => navigation.navigate('Save', { image })} /> */}
+      {/* {image && <Image source={{ uri: image }} style={{ flex: 1 }} />} */} 
+    </View>
+  );
 }
 
-export default Add
+
+// const styles = StyleSheet.create({
+//   cameraContainer: {
+//     flex: 1,
+//     flexDirection: 'row'
+//   },
+//   fixedRatio: {
+//     flex: 1,
+//     aspectRatio: 1
+//   }
+
+// })
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flexDirection:'column-reverse'
   },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    backgroundColor: 'transparent',
+    flexDirection: 'row',
+    margin: 20,
+    justifyContent:'space-between',
+    marginTop:"99%"
+  },
+  
 });
